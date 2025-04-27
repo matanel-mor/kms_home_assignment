8c83c2961c6a2bd2ccff49e445085591f72634ed
matanel-mor <matanelmm1997@gmail.com>
2025-04-27 19:33:01 +0300

    remove unnecessary beforeEach

--- src/tests/kms_university.spec.ts
+++ src/tests/kms_university.spec.ts
@@ -2,18 +2,15 @@ import { test } from '@playwright/test';
 import logger from '../utils/Logger';
 import HomePage from '../pages/homePage';
 
-test.beforeEach(async ({ page }) => {
-      const homePage = new HomePage(page);
-      await homePage.navigateToHomePage();
-      await homePage.assertNavigationToHomePage();
-});
-
 test('KMS university apply for certificate form', async ({ page }) => {
       logger.info("------Starting KMS university test-------");
 
       const homePage = new HomePage(page);
-      const lightHouseUniversityPage = await homePage.navigateToKMSUniversityPage();
+      await homePage.navigateToHomePage();
+      await homePage.assertNavigationToHomePage();
 
+      // Page transition to KMS University
+      const lightHouseUniversityPage = await homePage.navigateToKMSUniversityPage();
       await lightHouseUniversityPage.assertNavigationToKMSUniversityPage();
       await lightHouseUniversityPage.selectRandomCertificationCard();
       await lightHouseUniversityPage.assertFormFieldsAreVisible();
