525c5fa815f29d05aaea264ad0422b13c86124f5
matanel-mor <matanelmm1997@gmail.com>
2025-04-28 19:01:09 +0300

    terms ans privacy test

---
+++ docs/context.md
@@ -0,0 +1,7 @@
+## Explanation
+
+I selected these two test cases because navigation to key informational pages (like Terms and Privacy Policy) and validation of critical user forms (like the university application form) are fundamental to ensuring user trust, legal compliance, and a smooth user experience. Both are high-impact areas that directly affect user engagement and platform credibility.
+
+## Additional Note
+
+Many pages in the app share a similar structure and behavior. I could have chosen to test pages like **Guides** or **Webinars** instead of the University page, or selected another **Legal Policy** page. Since I can't test the entire app... I chose to focus on one more form that allows us to get more clients and user engagement + Legal page that are vital for our company legal coverage.
---
+++ src/pages/termsAndPrivacyPage.ts
@@ -0,0 +1,22 @@
+import { Page, expect } from "@playwright/test";
+import logger from "../utils/Logger";
+import { termsAndPrivacyPageData } from "../data/termsAndPrivacy";
+
+export default class TermsAndPrivacyPage {
+      constructor(public page: Page) { }
+
+      // Selectors
+      get termsAndPrivacyTitle() {
+            return this.page.locator('h1');
+      };
+
+      // Assertions
+      async assertNavigationToTermsAndPrivacyPolicyPage() {
+            await expect(this.page).toHaveURL(`${process.env.BASE_URL}terms-and-privacy-policy/`);
+            logger.info(`Navigated to terms and privacy policy page.`);
+      };
+      async assertTermsAndPrivacyPolicyTitle() {
+            await expect(this.termsAndPrivacyTitle).toBeVisible();
+            await expect(this.termsAndPrivacyTitle).toHaveText(termsAndPrivacyPageData.title);
+      };
+};
